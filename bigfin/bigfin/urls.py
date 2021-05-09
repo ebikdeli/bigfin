6fc36c304f6f48fc036120eeb8deca3f30353aad
ebikdeli <ebikdeli@gmail.com>
2021-05-09 18:02:45 +0430

    wagtail integrated into project

--- bigfin/bigfin/urls.py
+++ bigfin/bigfin/urls.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.core import urls as wagtail_urls
+from wagtail.documents import urls as wagtaildocs_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cms/', include(wagtailadmin_urls)),
+    path('documents/', include(wagtaildocs_urls)),
+    path('pages/', include(wagtail_urls)),
+    path('', include(wagtail_urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
